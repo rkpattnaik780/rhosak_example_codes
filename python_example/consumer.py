53353ab0428869507c47d40552a086dc9a8be671
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2022-12-01 11:55:08 +0530

    fix: address review comments

--- python_example/consumer.py
+++ python_example/consumer.py
@@ -8,27 +8,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+kafka_host = os.getenv("KAFKA_HOST")
+rhoas_client_id = os.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_ID")
+rhoas_client_secret = os.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET")
+rhoas_oauth_token_url = os.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL")
+
+
 def _get_token(config):
-    payload = {
-        'grant_type': 'client_credentials',
-        'scope': 'api.iam.service_accounts'
-    }
-    resp = requests.post(os.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL"),
-                         auth=(os.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_ID"), os.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET")),
-                         data=payload)
+    payload = {"grant_type": "client_credentials", "scope": "api.iam.service_accounts"}
+    resp = requests.post(
+        rhoas_oauth_token_url,
+        auth=(
+            rhoas_client_id,
+            rhoas_client_secret,
+        ),
+        data=payload,
+    )
     token = resp.json()
-    return token['access_token'], time.time() + float(token['expires_in'])
+    return token["access_token"], time.time() + float(token["expires_in"])
 
 
 topic = "prices"
 consumer_conf = {
-    'bootstrap.servers': os.getenv("KAFKA_HOST"),
-    'security.protocol': 'SASL_SSL',
-    'sasl.mechanisms': 'OAUTHBEARER',
+    "bootstrap.servers": kafka_host,
+    "security.protocol": "SASL_SSL",
+    "sasl.mechanisms": "OAUTHBEARER",
     "group.id": "test-group",
-    'session.timeout.ms': 6000,
-    'auto.offset.reset': 'earliest',
-    'oauth_cb': _get_token,
+    "session.timeout.ms": 6000,
+    "auto.offset.reset": "earliest",
+    "oauth_cb": _get_token,
 }
 consumer = Consumer(consumer_conf)
 
--- python_example/producer.py
+++ python_example/producer.py
@@ -8,11 +8,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+kafka_host = os.getenv("KAFKA_HOST")
+rhoas_client_id = os.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_ID")
+rhoas_client_secret = os.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET")
+rhoas_oauth_token_url = os.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL")
+
+
 def _get_token(config):
     payload = {"grant_type": "client_credentials", "scope": "api.iam.service_accounts"}
     resp = requests.post(
-        os.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL"),
-        auth=(os.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_ID"), os.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET")),
+        rhoas_oauth_token_url,
+        auth=(rhoas_client_id, rhoas_client_secret),
         data=payload,
     )
     token = resp.json()
@@ -20,7 +26,7 @@ def _get_token(config):
 
 topic = "prices"
 producer_conf = {
-    "bootstrap.servers": os.getenv("KAFKA_HOST"),
+    "bootstrap.servers": kafka_host,
     "security.protocol": "SASL_SSL",
     "sasl.mechanisms": "OAUTHBEARER",
     "oauth_cb": _get_token,
