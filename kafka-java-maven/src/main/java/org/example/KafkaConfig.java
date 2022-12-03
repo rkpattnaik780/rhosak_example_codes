51cc17684f807247d682d04a65e6eb6a2333111d
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2022-12-03 12:28:09 +0530

    fix: use camel case for variables

--- kafka-java-maven/src/main/java/org/example/KafkaConfig.java
+++ kafka-java-maven/src/main/java/org/example/KafkaConfig.java
@@ -8,21 +8,21 @@ public class KafkaConfig {
 
     static Properties properties() {
 
-        String KAFKA_HOST = System.getenv("KAFKA_HOST");
-        String RHOAS_CLIENT_ID = System.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_ID");
-        String RHOAS_CLIENT_SECRET = System.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET");
-        String RHOAS_OAUTH_TOKEN_URL = System.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL");
+        String kafkaHost = System.getenv("KAFKA_HOST");
+        String rhoasClientID = System.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_ID");
+        String rhoasClientSecret = System.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET");
+        String rhoasOauthTokenUrl = System.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL");
 
         var properties= new Properties();
 
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_HOST);
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost);
         properties.setProperty("security.protocol", "SASL_SSL");
         properties.setProperty("sasl.mechanism", "OAUTHBEARER");
 
-        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required clientId=\"" + RHOAS_CLIENT_ID + "\" clientSecret=\"" + RHOAS_CLIENT_SECRET + "\" oauth.token.endpoint.uri=\"" + RHOAS_OAUTH_TOKEN_URL + "\";");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required clientId=\"" + rhoasClientID + "\" clientSecret=\"" + rhoasClientSecret + "\" oauth.token.endpoint.uri=\"" + rhoasOauthTokenUrl + "\";");
 
         properties.setProperty("sasl.login.callback.handler.class", "org.apache.kafka.common.security.oauthbearer.secured.OAuthBearerLoginCallbackHandler");
-        properties.setProperty("sasl.oauthbearer.token.endpoint.url", RHOAS_OAUTH_TOKEN_URL);
+        properties.setProperty("sasl.oauthbearer.token.endpoint.url", rhoasOauthTokenUrl);
         properties.setProperty("sasl.oauthbearer.scope.claim.name", "api.iam.service_accounts");
 
         return properties;
