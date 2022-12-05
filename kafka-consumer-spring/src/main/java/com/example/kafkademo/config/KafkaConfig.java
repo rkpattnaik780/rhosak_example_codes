01d558faaa9db31c29d25523e8f3c787b703d7ac
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2022-12-05 12:32:16 +0530

    fix: move accessing environment variables

--- kafka-consumer-spring/src/main/java/com/example/kafkademo/config/KafkaConfig.java
+++ kafka-consumer-spring/src/main/java/com/example/kafkademo/config/KafkaConfig.java
@@ -21,17 +21,19 @@ public class KafkaConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        var kafkaClientId = System.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_ID");
-        var kafkaClientSecret = System.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET")
+        String kafkaHost = System.getenv("KAFKA_HOST");
+        String rhoasClientID = System.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_ID");
+        String rhoasClientSecret = System.getenv("RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET");
+        String rhoasOauthTokenUrl = System.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL");
 
-        config.put("bootstrap.servers", System.getenv("KAFKA_HOST"));
+        config.put("bootstrap.servers", kafkaHost);
 
         config.put("security.protocol", "SASL_SSL");
         config.put("sasl.mechanism", "OAUTHBEARER");
 
-        config.put("sasl.jaas.config", "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required clientId=\"" + kafkaClientId + "\" clientSecret=\"" + kafkaClientSecret + "\" oauth.token.endpoint.uri=\"" + System.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL") + "\";");
+        config.put("sasl.jaas.config", "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required clientId=\"" + rhoasClientID + "\" clientSecret=\"" + rhoasClientSecret + "\" oauth.token.endpoint.uri=\"" + rhoasOauthTokenUrl + "\";");
         config.put("sasl.login.callback.handler.class", "org.apache.kafka.common.security.oauthbearer.secured.OAuthBearerLoginCallbackHandler");
-        config.put("sasl.oauthbearer.token.endpoint.url", System.getenv("RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL"));
+        config.put("sasl.oauthbearer.token.endpoint.url", rhoasOauthTokenUrl);
         config.put("sasl.oauthbearer.scope.claim.name", "api.iam.service_accounts");
 
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
